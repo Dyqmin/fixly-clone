e0db0c1ec42dba751b44ba9c4f1d1b7b822fee4d
Domin <teesdomin@gmail.com>
2021-02-04 23:16:56 +0100

    fix env variables

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def create_app(config_file=None, settings_override=None):
 
     @app.route('/')
     def hello():
-        return 'Hello world!'
+        return str(POSTGRES_DB)
 
     db.init_app(app)
     migrate.init_app(app, db)
