0514f6f3d476cc8e00a1e42adf22434d366c700a
Domin <teesdomin@gmail.com>
2021-02-11 23:31:20 +0100

    seperate api init lines

--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, Blueprint
 from src.db import db, migrate
 import os
-from src.services import users, user_types, login, order_types, orders, offers
-from flask_restplus import Api, Resource
-
+from src.api import init_api
 
 POSTGRES_DB = os.environ.get('POSTGRES_DB')
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
@@ -17,18 +15,7 @@ def create_app(config_file=None, settings_override=None):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    api = Api(version='1.0', title='Fixly clone',
-              description='WIT Project for Python basics course')
-    api.init_app(app)
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
-    api.init_app(blueprint)
-    api.add_namespace(login.login)
-    api.add_namespace(users.users)
-    api.add_namespace(order_types.order_types)
-    api.add_namespace(user_types.user_types)
-    api.add_namespace(orders.orders)
-    api.add_namespace(offers.offers)
-    app.register_blueprint(blueprint)
+    init_api(app)
 
     return app
 
